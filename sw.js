c49294699bc7dab44329d576dfcc27bee129437b
Francis Sunday <iamfrankcute@gmail.com>
2017-06-13 02:28:29 +0100

    New Draft, Migrating to a PWA-30%

---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener("push", function(event) {
+    event.waitUntil(
+        self.registration.showNotification('Got Push?', {
+            body: 'Push Message recieved'
+        })
+    );
+});
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('the-magic-cache').then(function(cache) {
+        return cache.addAll([
+            '/',
+            '/index.html',
+
+        ]);
+    })
+    );
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
