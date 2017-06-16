e9503bc12270b2f525306f5178e48d47ff467f28
Francis Sunday <iamfrankcute@gmail.com>
2017-06-16 21:55:00 +0100

    Update Service Worker

--- sw.js
+++ sw.js
@@ -1,3 +1,32 @@
+var cacheName = 'hakaseBlog-1';
+var filesToCache = [
+        '/',
+        '/index.html',
+        '/assets/',
+        '/public/css/hyde.css',
+        '/public/css/poole.css',
+        '/public/css/syntax.css',
+        '/2017/',
+    ];
+
+// activate the service worker
+self.addEventListener('activate', function(e) {
+    console.log('[*] Service worker activated');
+    e.waitUntil(
+        caches.keys()
+        .then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[*] ServiceWorker - Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+// Push Notifications
 self.addEventListener("push", function(event) {
     event.waitUntil(
         self.registration.showNotification('Got Push?', {
@@ -6,22 +35,16 @@ self.addEventListener("push", function(event) {
     );
 });
 
+// When the server worker installs
 self.addEventListener('install', function(e) {
     e.waitUntil(
-        caches.open('the-magic-cache').then(function(cache) {
-        return cache.addAll([
-            '/',
-            '/index.html',
-            '/*.html',
-            '/*.css',
-            '/*.js',
-            '/*.md'
-
-        ]);
+        caches.open(cacheName).then(function(cache) {
+        return cache.addAll(filesToCache);
     })
     );
 });
 
+// Fetch Cached items 
 self.addEventListener("fetch", function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
