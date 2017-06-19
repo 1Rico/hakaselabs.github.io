54c59076754f8299230b2ca959e062ae608f883e
Francis Sunday <francis@hakaselabs.io>
2017-06-19 23:02:40 +0100

    update service worker

--- sw.js
+++ sw.js
@@ -43,9 +43,43 @@ self.addEventListener('install', function(e) {
 
 // Fetch Cached items
 self.addEventListener("fetch", function(event) {
+    console.log('The service worker is serving the assets');
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
+        fromCache(event.request);
+    );
+    event.waitUntil(
+        update(event.request)
+        .then(refresh)
     );
 });
+
+function fromCache(request) {
+    return caches.open(cacheName)
+            .then(function(cache) {
+                return cache.match(request);
+            });
+}
+function update(request) {
+    return caches.open(cacheName)
+            .then(function(cache) {
+                return fetch(request)
+                .then(function(response) {
+                    return cache.put(request, response.clone()).then( function() {
+                        return response;
+                    });
+                });
+            });
+}
+function refresh(response) {
+    return self.clients.matchAll()
+    .then(function(clients) {
+        clients.forEach(function(client) {
+            var message = {
+                type: 'refresh',
+                url: response.url,
+                eTag: response.headers.get('ETag')
+            };
+            client.postMessage(JSON.stringify(message));
+        });
+    });
+}
