526604604061814ba11f537951a7cc9fedb25ded
Francis Sunday <francis@hakaselabs.io>
2017-06-24 00:37:03 +0100

    New Post

--- site.js
+++ site.js
@@ -1,9 +1,9 @@
 navigator.serviceWorker && navigator.serviceWorker.register('/sw.js').then(function(registration) {
   console.log('Excellent, registered with scope: ', registration.scope);
 });
-navigator.serviceWorker && navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {  
-  serviceWorkerRegistration.pushManager.getSubscription()  
-    .then(function(subscription) {  
+navigator.serviceWorker && navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+  serviceWorkerRegistration.pushManager.getSubscription()
+    .then(function(subscription) {
       if (subscription) {
         console.info('Got existing', subscription);
       }
